674950da1b294b5d3e13e4947f4f3915508e1ef0
YangMann <zhangyang93@rocketmail.com>
2013-10-20 14:16:35 +0800

    Initial commit

--- bluetooth-test/src/edu/SJTU/zhangyang/bluetooth_test/MyActivity.java
+++ bluetooth-test/src/edu/SJTU/zhangyang/bluetooth_test/MyActivity.java
@@ -1,7 +1,10 @@
 package edu.SJTU.zhangyang.bluetooth_test;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +14,19 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        TextView textView1 = (TextView) findViewById(R.id.text1);
+        String status = "";
+        if (bluetoothAdapter != null) {
+            if (!bluetoothAdapter.isEnabled()) {
+                bluetoothAdapter.enable();
+            }
+            String myDeviceAddress = bluetoothAdapter.getAddress();
+            String myDeviceName = bluetoothAdapter.getName();
+            status = myDeviceName + ": " + myDeviceAddress;
+        }
+        textView1.setText(textView1.getText() + "\n" + status);
+        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
     }
 }
